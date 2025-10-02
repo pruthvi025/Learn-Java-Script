1a0be64ccc27c1eb6b15cd02baf7fa16d68d6017
Pruthviraj Ankush Thorbole <pruthvirajthorbole96@gmail.com>
2025-10-02 17:41:24 +0000

    learned conversion

---
+++ Basics_javaScript/CoversionAndOperation.js
@@ -0,0 +1,36 @@
+let number="33" // it is an  string
+
+console.log(number)
+
+let ConvertToNumber=Number(number) // Number is function wich is used to convert to number format
+console.log( number)
+console.log(typeof number)//converted to srting 
+
+console.log("-----------------------------------")
+
+let num2="33abc"
+
+let ConvertToNumber1=Number(num2)
+console.log(typeof ConvertToNumber1)
+console.log( ConvertToNumber1) // print NaN means Not A Number 
+
+
+console.log("-----------------------------------")
+
+let present= 1
+
+let ConvertToBoolean=Boolean(present)
+
+console.log(typeof ConvertToBoolean)
+console.log( ConvertToBoolean)
+
+// let name=" " after converting empty string to boolean it will give output false
+// let name"pruthvi" after converting any string to boolean it will give output true
+
+console.log("-----------------------------------")
+
+let p=null
+
+
+
+
