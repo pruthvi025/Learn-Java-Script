81927ff584fe711f9baa0568a5783ce58d6c66cb
Pruthviraj Ankush Thorbole <pruthvirajthorbole96@gmail.com>
2025-10-02 16:56:11 +0000

    learn data type and variable

---
+++ Basics_javaScript/DataTypesAndECMA.js
@@ -0,0 +1,11 @@
+"use strict"
+// use strict is used to treat the all js code as a newer version 
+
+//alert(pruthvi)//it is used in browser we are using node js so it will not work in here
+let name="pruthvi" //string type
+let num=10 //number or integer type
+let present = true // boolean type
+
+console.log(typeof name); //print the datatype of veriable
+console.log(typeof null)// print as objectl
+console.log(present)
---
+++ Basics_javaScript/veriable.js
@@ -0,0 +1,11 @@
+const number=2356;
+let name="Pruthviraj thorbole"
+//number=54 //we already declare this veriable with const keyword so it will create a error
+{//this is an scope or block of code
+    let name = "sahil"
+    console.log(name);
+}   
+
+console.log(number);
+console.table([number , name])// print the all details in tabular form 
+
